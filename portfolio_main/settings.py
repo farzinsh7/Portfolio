03a957a63ffdda39fda6d6e40236a2238ed789d1
EG <157224267+EG-style@users.noreply.github.com>
2024-06-26 09:35:40 +0330

    Add Xframe Permission

--- portfolio_main/settings.py
+++ portfolio_main/settings.py
@@ -126,3 +126,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static-cdn', 'static-root')
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static-cdn', 'media-root')
+
+X_FRAME_OPTIONS = 'ALLOWALL'
