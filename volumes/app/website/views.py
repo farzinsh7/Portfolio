7e2285fd857c00e682e75ccc64686759a8707b96
Farzin Shams <farzinnater@gmail.com>
2025-02-01 16:48:36 +0330

    Get the age by birth_date

--- volumes/app/website/views.py
+++ volumes/app/website/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, DetailView
+from django.utils.timezone import now
+from datetime import date
 from . import models
 
 
@@ -17,6 +19,18 @@ class AboutView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['skills'] = models.Skills.objects.all()
+
+        # Get the object instance
+        my_info = self.get_object()
+        if my_info and my_info.birth_date:
+            today = date.today()
+            age = today.year - my_info.birth_date.year - (
+                (today.month, today.day) < (
+                    my_info.birth_date.month, my_info.birth_date.day)
+            )
+            context['age'] = age
+        else:
+            context['age'] = None
 
         return context
 
