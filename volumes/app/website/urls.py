ffb0be73baa79fb7defc45a0cd537d9b2de727bb
Farzin Shams <farzinnater@gmail.com>
2025-02-01 12:08:27 +0330

    Added about and contact template

--- volumes/app/website/urls.py
+++ volumes/app/website/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = "website"
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
-    # path("about", views.AboutView.as_view(), name="about"),
-    # path("contact", views.ContactView.as_view(), name="contact"),
+    path("about/", views.AboutView.as_view(), name="about"),
+    path("contact/", views.ContactView.as_view(), name="contact"),
 ]
--- volumes/app/website/views.py
+++ volumes/app/website/views.py
@@ -4,3 +4,11 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = "website/index.html"
+
+
+class AboutView(TemplateView):
+    template_name = "website/about.html"
+
+
+class ContactView(TemplateView):
+    template_name = "website/contact.html"
