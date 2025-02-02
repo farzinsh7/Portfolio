6b59c4aae691513a0ec0a7a24aff4a84834bcdba
Farzin Shams <farzinnater@gmail.com>
2025-02-02 11:08:08 +0330

    Creating resume models & admin view

--- volumes/app/portfolio/admin.py
+++ volumes/app/portfolio/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class ResumeGalleryadmin(admin.TabularInline):
+    model = models.ResumeGallery
+    extra = 1
+
+
+@admin.register(models.Resume)
+class ResumeAdmin(admin.ModelAdmin):
+    inlines = [ResumeGalleryadmin]
+
+
+admin.site.register(models.ResumeCategory)
--- volumes/app/portfolio/models.py
+++ volumes/app/portfolio/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class ResumeCategory(models.Model):
+    title = models.CharField(max_length=120)
+    name = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.title
+
+
+class Resume(models.Model):
+    title = models.CharField(max_length=150, default='title')
+    description = models.TextField(default='description')
+    client = models.CharField(max_length=150, default='user')
+    website = models.CharField(max_length=150, null=True)
+    project_date = models.DateField(null=True)
+    thumbnail = models.ImageField(upload_to="resume/thumbnail")
+    categories = models.ManyToManyField(ResumeCategory)
+    created_date = models.DateTimeField(auto_now_add=True, null=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class ResumeGallery(models.Model):
+    title = models.CharField(max_length=150)
+    image = models.ImageField(upload_to="resume/gallery")
+    gallery = models.ForeignKey(
+        Resume, on_delete=models.CASCADE, null=True, related_name='gallery')
+
+    def __str__(self):
+        return self.title
