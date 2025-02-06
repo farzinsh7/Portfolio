5aec3fdd4746e09d1df203786ec01cffc681e5cc
Farzin Shams <farzinnater@gmail.com>
2025-02-06 18:28:52 +0330

    Start dashboard app

--- volumes/app/accounts/views.py
+++ volumes/app/accounts/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.contrib.auth import views as auth_view
 from django.contrib.auth.forms import AuthenticationForm
 
@@ -6,7 +5,7 @@ from django.contrib.auth.forms import AuthenticationForm
 class LoginView(auth_view.LoginView):
     form_class = AuthenticationForm
     template_name = "accounts/login.html"
-    redirect_authenticated_user = False
+    redirect_authenticated_user = True
 
 
 class LogoutView(auth_view.LogoutView):
