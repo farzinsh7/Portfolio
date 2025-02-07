74aad4575d550c005d540405029af0b0a548c2f2
Farzin Shams <farzinnater@gmail.com>
2025-02-07 20:08:31 +0330

    Added Avatar to User model

--- volumes/app/accounts/models.py
+++ volumes/app/accounts/models.py
@@ -1,7 +1,11 @@
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
 from django.db import models
+from django.dispatch import receiver
+from django.utils.translation import gettext_lazy as _
 
 
-# Create your models here.
 class User(AbstractUser):
     email = models.EmailField(unique=True)
+    avatar = models.ImageField(
+        upload_to="profile/", default="profile/default.jpg")
